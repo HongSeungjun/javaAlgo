13924c7ba355f18e8bd367e98c84db1d5902eb59
ghdgn <ghdgn22@gmail.com>
2022-03-25 15:53:54 +0900

    4344 soution

--- 1110/src/Main.java
+++ 1110/src/Main.java
@@ -7,31 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         int N;
-        int firisDigit, secondDigit;
-        int check = -1;
+        int check = 0;
         int count = 0;
-        int temp = 0;
-        int t= 0;
 
         BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
 
         N = Integer.parseInt(br.readLine());
-        temp = N;
-        while(N != check) {
-            if (temp < 10) {
-                firisDigit = 0;
-                secondDigit = temp;
-            } else {
-                firisDigit = temp / 10;
-                secondDigit = temp % 10;
-            }
-            if (firisDigit+secondDigit >= 10){
-                t = (firisDigit+secondDigit)%10;
-                temp= (secondDigit*10) + t;
-            }else temp= (secondDigit*10) + (firisDigit+secondDigit);
 
-            check = temp;
-            count ++;
+        check = N;
+        while(((check%10)*10+((check+check/10)%10))!=N) {
+
+            count++;
+
         }
         System.out.println(count);
     }
